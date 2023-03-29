1581972879fc51f5784890d729c966b8fc7c4666
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-03-29 10:14:34 +0100

    email button

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -6,6 +6,7 @@ const Contact = () => {
     <div className="container contact__container">
       <h2>Contact</h2>
       <h4>Email me at: nabeel.anwar01@gmail.com</h4>
+      <a href="mailto:nabeel.anwar01@gmail.com" className="btton">Click to Send an Email</a>
       <br/>
       <h4>Give me a call at: 07897897683</h4>
     </div>

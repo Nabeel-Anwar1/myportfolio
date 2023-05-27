788d563b140a04889db834e0ff8d230f72ea06f1
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-05-27 14:08:51 +0100

    changed image tags on snooker proj

--- src/components/projects/SnookerScorer.jsx
+++ src/components/projects/SnookerScorer.jsx
@@ -27,10 +27,10 @@ Snooker Scoremaster is more than just a scoring app; it's your trusty companion 
 
 I started by discussing what the app should look like, and I was given a design made on Figma that I should mirror.<br />
 
-<img src={figure1} alt="Figma design of first screen" /><br />
+<img src={figure1} alt="Figma design of first screen" className="snookerimg"/><br />
 Figure 1 - Figma design that I was tasked with recreating in React Native.<br />
 
-<img src={figure2} alt="Figma design of second screen" /><br />
+<img src={figure2} alt="Figma design of second screen" className="snookerimg"/><br />
 Figure 2 - Figma design of the popup "calculator" to add, subtract or end the frame.<br />
 
 I first created a react-expo app, then went onto start the scores. I wanted to create two boxes that were clickable, with text in the middle. As I wanted to have more control over the look of the button and not have fixed settings, I used Pressable instead of Button. I created the pressables, which initially had no function, then added colour and text to them.<br /><br />
@@ -38,10 +38,10 @@ I first created a react-expo app, then went onto start the scores. I wanted to c
 During a project I completed as part of a group, I learned about Modals and how to use them. I made the pressables able to view the modal which contained the "calculator". The calculator was made from scratch, each number was a pressable that updated the calculators screen using the useState function. Finally, the buttons at the bottom of the calculator for add and subtract had their own functions, and the new game button compared the scores and added 1 to the frame score of the player.<br />
 
 At this current stage, the app looks like this:
-<img src={figure3} alt="First Screen in app" /> <br />
+<img src={figure3} alt="First Screen in app" className="snookerimg"/> <br />
 Figure 3 - When the app launches you are greeting by the two scores inside of buttons.<br />
 
-<img src={figure4} alt="Second Screen in app" /><br />
+<img src={figure4} alt="Second Screen in app" className="snookerimg"/><br />
 Figure 4 - This shows what happens when you click on a players score, you are able to input numbers and add or subtract. The new game button checks the difference between scores and adds 1 to the frames score on the first screen to whoever had the highest score.<br />
     </p>
     <a href="https://github.com/Nabeel-Anwar1/SnookerScorer" target="_blank" rel="noopener noreferrer" className="btton">GitHub</a>

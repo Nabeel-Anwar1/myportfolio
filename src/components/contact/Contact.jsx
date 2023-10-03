8a3bfc844a95a06570596547359b2f7df5ddb6a2
Nabeel-Anwar1 <nabeel.anwar01@gmail.com>
2023-10-03 20:15:34 +0100

    changes to styling, added in scroll snap, fixed some styling issues, some devices the projects section was overflowing so added a overflow scroll

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -10,7 +10,6 @@ const Contact = () => {
           <a href="mailto:nabeel.anwar01@gmail.com" className="btton">
             Click to Send an Email
           </a>
-          <br />
           <h4>Give me a call at: 07897897683</h4>
         </div>
       </div>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,40 +5,42 @@ import { AiFillGithub } from "react-icons/ai";
 
 const Header = () => {
   return (
-    <header id="header">
-      <div className="container header__container">
-        {/* <div className="header__profile">
+    <section id="header">
+      <header id="header">
+        <div className="container header__container">
+          {/* <div className="header__profile">
           <img src={Me} alt="Nabeel" className="MeImg"/>
         </div> */}
-        <h3>Nabeel Anwar</h3>
-        <p>Software Developer</p>
-        <small>JavaScript | REACT | Node.js | Express.js | PSQL | Jest</small>
-        <div className="header__cta">
-          <a href="#contact" className="Button primary">
-            Get in contact!
-          </a>
-          <a href="#projects" className="Button primary">
-            My Work
-          </a>
+          <h3>Nabeel Anwar</h3>
+          <p>Software Developer</p>
+          <small>JavaScript | REACT | Node.js | Express.js | PSQL | Jest</small>
+          <div className="header__cta">
+            <a href="#contact" className="Button primary">
+              Get in contact!
+            </a>
+            <a href="#projects" className="Button primary">
+              My Work
+            </a>
+          </div>
+          <div className="header__socials">
+            <a
+              href="https://www.linkedin.com/in/nabeel-anwar/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <AiFillLinkedin size={"3rem"} />
+            </a>
+            <a
+              href="https://github.com/nabeel-anwar1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {<AiFillGithub size={"3rem"} />}
+            </a>
+          </div>
         </div>
-        <div className="header__socials">
-          <a
-            href="https://www.linkedin.com/in/nabeel-anwar/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <AiFillLinkedin size={"3rem"} />
-          </a>
-          <a
-            href="https://github.com/nabeel-anwar1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {<AiFillGithub size={"3rem"} />}
-          </a>
-        </div>
-      </div>
-    </header>
+      </header>
+    </section>
   );
 };
 
